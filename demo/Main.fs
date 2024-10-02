7aa4440407b237f7cf0dec90cc9a62e755c23cb9
Maxime Mangel <mangel.maxime@protonmail.com>
2024-10-02 22:38:45 +0200

    chore: make the demo run

--- demo/Main.fs
+++ demo/Main.fs
@@ -1,3 +1,13 @@
 module Demo.Main
 
-printfn "Hello, from the demo!"
+open Fable.Core.JsInterop
+open Antidote.FormStudio.DynamicFormDesigner
+open Feliz
+open Browser
+
+importSideEffects "../node_modules/bulma/css/bulma.min.css"
+
+let root = ReactDOM.createRoot(document.getElementById "root")
+root.render(
+    DynamicFormDesigner StandAlone
+)
